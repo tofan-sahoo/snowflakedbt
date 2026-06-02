166d772c9645dd8b7839e207ba90e799ce1e6fb7
tofan-sahoo <tofan_sahoo@yahoo.com>
2026-06-02 21:34:59 +0000

    dbt setup done for developer isolation

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,11 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {# 1. Identify the executing environment target (e.g., dev, prod, staging) #}
+    {# 1. Identify the executing environment target (e.g., dev, sit, ppt, prod) #}
     {%- set default_schema = target.schema -%}
     
     {# 2. Capture the currently logged-in Snowflake user and clean the string #}
-    {%- set user_name = target.user | trim | lower -%}
-
+    {%- set user_name = var('dbt_user',target.user) | default('') | trim | lower -%}
+        
     {# 3. Production/Deployment Isolation Logic #}
     {%- if target.name in ['prod', 'sit', 'ppt'] -%}
         
@@ -17,24 +17,17 @@
 
     {# 4. Developer / Snowsight Workspace Isolation Logic #}
     {%- else -%}
-
-        {{ log('inside target dev' ,True) }}
-        
+       
         {# Fallback to default schema if user variable isn't captured safely #}
         {%- if user_name is not none and user_name != '' -%}
-
-            {{ log('inside user provided in the profile' ,True) }}
-            
+           
             {%- if custom_schema_name is none -%}
-                {{ log('inside empty custom schema' ,True) }}            
-                dev_{{ user_name }}
+                dbt_{{ user_name }}
             {%- else -%}
-                {{ log('inside custom schema has value' ,True) }}            
-                dev_{{ user_name }}_{{ custom_schema_name | trim }}
+                dbt_{{ user_name }}_{{ custom_schema_name | trim }}
             {%- endif -%}
             
         {%- else -%}
-            {{ log('inside user not provided in the profile' ,True) }}        
             {{ default_schema }}
         {%- endif -%}
 
--- target/run/tasty_bytes/models/marts/testmodel.sql
+++ target/run/tasty_bytes/models/marts/testmodel.sql
@@ -2,7 +2,7 @@
   
     
 
-        create or replace transient table TASTY_BYTES_DBT_DB.tofansahoo_dev_data_mart.testmodel
+        create or replace transient table TASTY_BYTES_DBT_DB.dbt_tofansahoo_data_mart.testmodel
          as
         (select 1 as test
         );
